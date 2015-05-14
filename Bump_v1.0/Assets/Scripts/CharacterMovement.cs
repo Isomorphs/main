158a4bfaca1897fbd662d713be1bafb8e1c67d23
Maggie <maggieliyd@gmail.com>
2015-05-14 13:32:56 +0800

    Updated character movement

---
+++ Bump_v1.0/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public float jumpSpeed = 10f;
+	public float mouseSensitivity = 60f;
+	public float verticalRange = 80f;
+
+	private Vector3 movement;
+	private Rigidbody playerRB;
+	private float rotV = 0f;
+	private bool grounded = true;
+
+	// Use this for initialization
+	void Awake () {
+		playerRB = GetComponent<Rigidbody> ();
+	
+	}
+	
+
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		//onCollisionEnter (Collider info);
+
+		Jump ();
+	
+		Move (h,v);
+	}
+
+	void Move (float h, float v) {
+		float rotH = Input.GetAxis ("Mouse X") * mouseSensitivity;
+
+
+		rotV -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		rotV = Mathf.Clamp(rotV, -verticalRange, verticalRange);
+
+		Camera.main.transform.localRotation = Quaternion.Euler(rotV,0,0);
+
+
+		transform.Rotate(0,rotH,0);
+
+		movement.Set (h, 0f, v);
+		movement = transform.rotation * movement.normalized * speed * Time.deltaTime;
+
+		playerRB.MovePosition (transform.position + movement);
+	}
+
+	void Jump () {
+		if (Input.GetButtonDown ("Jump")) {
+			if (grounded) {
+			GetComponent<Rigidbody>().AddForce(Vector3.up * jumpSpeed);
+			//GetComponent<Rigidbody>().velocity += Vector3.up * j umpSpeed;
+				grounded = false;
+			}
+		}
+
+		else grounded = true;
+	}
+	
+	void onCollisionEnter (Collider info) {
+		if (info.tag == "Floor")
+			grounded = true;
+	}
+}
+	
---
+++ Bump_v1.0/Assets/Scripts/ColorChange.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour {
+	public Color colorCurrent;
+
+
+	// Use this for initialization
+	void Start () {
+		colorCurrent = GetComponent<Renderer>().material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.gameObject.GetComponent<Renderer>().material.color = Color.Lerp(colorCurrent, Color.white, Time.time/5000);
+		colorCurrent = GetComponent<Renderer>().material.color;
+	}
+
+	void OnTriggerEnter (Collider info) {
+
+		if(info.tag == "Player") {
+				this.gameObject.GetComponent<Renderer>().material.color =  Color.black;
+				colorCurrent = Color.black;
+		}
+
+	}
+}
