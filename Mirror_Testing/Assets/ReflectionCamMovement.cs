b4fc23013a148e3711b6c4daa6233003bfbcbaa3
Yunzhe <ericliyunzhe@gmail.com>
2015-05-14 13:23:23 +0800

    Initial Commit

---
+++ Mirror_Testing/Assets/ReflectionCamMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReflectionCamMovement : MonoBehaviour {
+
+	GameObject mirror;
+	Vector3 offset;
+	Vector3 rot;
+
+	// Use this for initialization
+	void Start () {
+		mirror = GameObject.Find ("Mirror");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset.x = Camera.main.transform.position.x;
+		offset.y = Camera.main.transform.position.y;
+		offset.z = mirror.transform.position.z * 2f - Camera.main.transform.position.z;
+
+		transform.position = offset;
+
+		Vector3 fwd = transform.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		if (Physics.Raycast (transform.position, fwd, out hit)) {
+			rot = hit.point;
+			Vector3 relativePos = rot - transform.position;
+			Quaternion rotation = Quaternion.LookRotation (relativePos);
+			transform.rotation = rotation;
+		}
+	}
+}
---
+++ Mirror_Testing/Assets/Scripts/movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+	
+	public float speed = 2.0f;
+	public float mouseSensitivity = 60f;
+	public float verticalRange = 80f;
+	
+	private Vector3 movementV;
+	private Rigidbody playerRB;
+	private float rotV = 0f;
+	
+	// Use this for initialization
+	void Awake () {
+		playerRB = GetComponent<Rigidbody> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		
+		Move (h,v);
+		playerRB.AddForce (Vector3.up * Input.GetAxisRaw ("Jump") * 20f);
+
+	}
+	
+	void Move (float h, float v) {
+		float rotH = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		
+		
+		rotV -= Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		rotV = Mathf.Clamp(rotV, -verticalRange, verticalRange);
+		
+		Camera.main.transform.localRotation = Quaternion.Euler(rotV,0,0);
+		
+		
+		transform.Rotate(0,rotH,0);
+		
+		movementV.Set (h, 0f, v);
+		movementV = transform.rotation * movementV.normalized * speed * Time.deltaTime;
+		
+		playerRB.MovePosition (transform.position + movementV);
+	}
+}
