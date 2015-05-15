396f6fb851abf6418dbe350c82874ece32db4a14
Yunzhe <ericliyunzhe@gmail.com>
2015-05-15 23:25:44 +0800

    Implemented picking up objects problems using rigidbody components. Haven't added comments yet.

---
+++ Mirror_Testing/Assets/AddforceTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddforceTest : MonoBehaviour {
+
+	GameObject hand;
+	GameObject handle;
+	Quaternion camRot;
+	Rigidbody itemRB;
+	bool holding;
+	public Vector3 offset;
+	Vector3 forceDir;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+		itemRB = GetComponent<Rigidbody>();
+		hand = GameObject.FindWithTag("hand");
+		handle = GameObject.Find("Handle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump"))
+			holding = true;
+	}
+
+	void FixedUpdate(){
+		if (holding){
+			camRot = Camera.main.transform.rotation;
+			forceDir = hand.transform.position - handle.transform.position;
+			itemRB.AddForceAtPosition(forceDir * force, handle.transform.position);
+			itemRB.rotation = camRot;
+		}
+	}
+}
