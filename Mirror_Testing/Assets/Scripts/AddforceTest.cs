d13c7666e603de23a3476ec2186c58b217cb37da
Yunzhe <ericliyunzhe@gmail.com>
2015-05-19 16:17:37 +0800

    Completed 3d models of hollow cylinders

--- Mirror_Testing/Assets/Scripts/AddforceTest.cs
+++ Mirror_Testing/Assets/Scripts/AddforceTest.cs
@@ -7,8 +7,7 @@ public class AddforceTest : MonoBehaviour {
 	GameObject handle;
 	Quaternion camRot;
 	Rigidbody itemRB;
-	bool holding;
-//	public Vector3 offset;
+	bool holding = false;
 	Vector3 forceDir;
 	public float force;
 
@@ -21,18 +20,30 @@ public class AddforceTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//get user input (Space bar for picking up items)
 		if(Input.GetButtonDown("Jump"))
 		{
-			holding = true;
-			itemRB.useGravity = false;
-//			itemRB.mass = 0.01f;
+			if (holding){
+				holding = false;
+				itemRB.useGravity = true;
+			}
+			else {
+				holding = true;
+				//disable gravity for smooth movement
+				itemRB.useGravity = false;
+			}
 		}
 	}
 
 	void FixedUpdate(){
 		if (holding){
+			//get the direction to which the player is facing
 			camRot = Camera.main.transform.rotation;
+
+			//and direction of the force needed to be applied
 			forceDir = hand.transform.position - handle.transform.position;
+
+			//set transform of the item's rigidbody
 			itemRB.AddForceAtPosition(forceDir * force, handle.transform.position, ForceMode.Force);
 			itemRB.MoveRotation(camRot);
 		}
