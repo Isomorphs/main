f0d4bfb71f02f140f57e8f5d2471d6f131c29ee3
Yunzhe <ericliyunzhe@gmail.com>
2015-05-21 19:24:13 +0800

    miaooo

---
+++ Bump_v1.0/Assets/Scripts/LaserReceiverTest1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserReceiverTest1 : MonoBehaviour {
+	// the Raycasthit variable "hit" in the laser reflection script needs to be public
+	// Triggers should be tagged as "LaserTrigger"
+
+
+	GameObject laserSource;
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		laserSource = GameObject.Find ("Laser_Source");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (laserSource.GetComponentInChildren <Laser_Reflection> ().hit.collider.tag == "LaserTrigger")
+		{
+			transform.position = (transform.position + transform.up * speed * Time.deltaTime);
+			print ("Trigger works!"); // Should be replaced by more level-specific codes in the future
+		}
+	}
+}
