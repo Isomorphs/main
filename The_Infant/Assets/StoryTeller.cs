f7ac2b7dc37f27fdfc74c70d4187a8ab52c3264c
Maggie <maggieliyd@gmail.com>
2015-05-28 13:19:49 +0800

    Added Yunzhe's maze (clap clap).

---
+++ The_Infant/Assets/StoryTeller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryTeller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		GUI.color = Color.yellow;
+		GUI.TextField(new Rect(10, 10, 50, 50), "Hello World");
+	}
+}
