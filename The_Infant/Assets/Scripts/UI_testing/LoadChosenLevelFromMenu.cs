3b17b5c908942b193714c0871be08f945b7a6579
Yunzhe <ericliyunzhe@gmail.com>
2015-08-03 12:32:53 +0800

    Trmmed off unnecessary models

--- The_Infant/Assets/Scripts/UI_testing/LoadChosenLevelFromMenu.cs
+++ The_Infant/Assets/Scripts/UI_testing/LoadChosenLevelFromMenu.cs
@@ -9,13 +9,7 @@ public class LoadChosenLevelFromMenu : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//inputForGameLoading = GameObject.Find("LoadGameInput");
 		loadlevel = GameObject.Find("GameController").GetComponent<LoadLevel>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
 	//convert string to an int and load the selected level
